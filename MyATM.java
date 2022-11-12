e49fc8dd352f10acf72a3589efb08ca9b7c6f92f
leejoon5504 <51127756+leejoon5504@users.noreply.github.com>
2022-11-11 21:19:57 -0500

    Update MyATM.java

--- MyATM.java
+++ MyATM.java
@@ -1,10 +1,10 @@
 class MyATM {
   private static MyATM myATM = null;
-
+  
+  int balance;
+  
   private MyATM() { }
-
-  Card card;
-
+  
   public static MyATM getMyATM() {
     if (myATM == null) {
       myATM = new MyATM();
@@ -12,19 +12,73 @@ class MyATM {
     return myATM;
   }
 
-  public void insertCard(Card card) {
-    this.card = card;
-  }
+  Card card;
+  Account account;
 
-  public boolean validatePin(String pin) {
-    if (Server.validatePin(card, pin)) { // Send PIN to the server and validate
-      return true;
-    } else {
-      this.card = null;
-      return false;
+  public void insertCard(Card card) throws Exception {
+    try {
+      Server.readCard(card); // Read card from the server
+      this.card = card;
+    } catch (Exception e) {
+      throw e; // Failed to read the card
     }
   }
 
-  public Account selectAccount() {
+  public boolean validatePin(String pin) {
+    try {
+      if (Server.validatePin(this.card, pin)) { // Send PIN to the server and validate
+        return true;
+      } else {
+        return false;
+      }
+    } catch (Exception e) {
+      // Failed to validate the PIN
+    }
+    return false;
+  }
+
+  public boolean RetrieveAccount(String accountNum) throws Exception {
+    try {
+      this.account = Server.retrieveAccount(accountNum); // Retrieve account information from the server
+    } catch (Exception e) {
+      throw e;
+    }
+    return true;
+  }
+  
+  public Account selectAccount(String accountNum) throws Exception {
+    if (RetrieveAccount(accountNum)) { // Retrieve account information from the server
+      return this.account;
+    }
+    
+    return null;
+  }
+  
+  public int getBalance() {
+    return this.account.balance;
+  }
+  
+  public void deposit(int num) {
+    this.balance += num;
+    this.account.balance += num;
+    Server.updateBalance(this.account); // Let the server know
+  }
+  
+  public void withdraw(int num) {
+    if (this.balance >= num && this.account.balance >= num) {
+      this.balance -= num;
+      this.account.balance -= num;
+      Server.updateBalance(this.account); // Let the server know
+    }
+  }
+  
+  class Card {
+    public Card() { }
+  }
+  
+  class Account {
+    int balance;
+    
+    public Account() { }
   }
 }
